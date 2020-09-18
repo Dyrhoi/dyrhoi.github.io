c3fe73f4f37949aa11674901018b99eb7565cd3a
Dyrhoi <realarcoz@gmail.com>
2020-09-19 00:44:54 +0200

    Color changes, cool hover trick

--- app.js
+++ app.js
@@ -15,3 +15,61 @@ function getAge(dateString) {
     }
     return age;
 }
+
+const cursor = document.getElementsByClassName("cursor")[0]
+const cursorMedia = document.getElementsByClassName("cursor--media")[0]
+const backgroundHeaderCode = document.getElementById("header-bg-code");
+const backgroundClipPath = document.getElementById("backgroundclip");
+const backgroundClipPathCircle = backgroundClipPath.firstElementChild;
+
+let tween = null;
+
+
+gsap.set(".cursor", {xPercent: -50, yPercent: -50});
+gsap.set("#backgroundclip", {xPercent: -50, yPercent: -50});
+
+var cursorElement = cursor;
+var pos = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
+var mouse = { x: pos.x, y: pos.y };
+var speed = 0.1;
+
+var cursorXSet = gsap.quickSetter(cursorElement, "x", "px");
+var cursorYSet = gsap.quickSetter(cursorElement, "y", "px");
+
+var bgXSet = gsap.quickSetter(backgroundClipPath, "x", "px");
+var bgYSet = gsap.quickSetter(backgroundClipPath, "y", "px");
+
+
+window.addEventListener("mousemove", e => {
+    //cursor.style.transform = `translate3d(${e.clientX}px, ${e.clientY}px, 0)`;
+    mouse.x = e.x;
+    mouse.y = e.y;  
+});
+
+window.addEventListener("mouseover", e => {
+    if (e.target.classList.contains("coolfx")) {
+        if(tween != null) tween.kill();
+        tween = gsap.to(backgroundClipPathCircle, {r:200, cx: 200, cy: 200})
+    }
+});
+
+window.addEventListener("mouseout", e => {
+    if (e.target.classList.contains("coolfx")) {
+        if(tween != null) tween.kill();
+        tween = gsap.to(backgroundClipPathCircle, .3, {r:0, cx: 0, cy: 0})
+    }
+});
+
+gsap.ticker.add(() => {
+    var dt = 1.0 - Math.pow(1.0 - speed, gsap.ticker.deltaRatio()); 
+    
+    pos.x += (mouse.x - pos.x) * dt;
+    pos.y += (mouse.y - pos.y) * dt;
+    cursorXSet(pos.x);
+    cursorYSet(pos.y);
+
+    bgXSet(pos.x);
+    bgYSet(pos.y);
+    
+
+  });
